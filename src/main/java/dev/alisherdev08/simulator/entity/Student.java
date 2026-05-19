d4a3d271d4bf660f37e4a726e68c0964dd3cfb0e
shadow <githubalisherka@gmail.com>
2026-05-19 09:36:57 -0400

    Remove .gradle from git tracking and update gitignore

--- src/main/java/dev/alisherdev08/simulator/entity/Student.java
+++ src/main/java/dev/alisherdev08/simulator/entity/Student.java
@@ -1,4 +1,29 @@
 package dev.alisherdev08.simulator.entity;
 
+import java.util.UUID;
+
 public class Student {
+    private UUID id;
+    private String fullName;
+
+    public Student(UUID id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + fullName + '\'' +
+                '}';
+    }
 }
